8d9a390a8c8a03ebf1165741b8dcd157e2af4332
Daniil Kireev <imstarosta4web@gmail.com>
2023-11-30 14:53:25 +0300

    Test iter1 commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,56 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+var urlMap = make(map[string]string)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		postHandler(w, r)
+	case "GET":
+		getHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	url := string(body)
+	shortURL := generateShortURL(url)
+	urlMap[shortURL] = url
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "http://localhost:8080/%s", shortURL)
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	originalURL, ok := urlMap[id]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func generateShortURL(url string) string {
+	// Для простоты в этом примере просто возвращаем первые 8 символов оригинального URL.
+	return url[:8]
+}
