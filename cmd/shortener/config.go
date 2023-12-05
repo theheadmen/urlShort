77f2b07734edd90d605fbbf796e0a0e2ce4c718e
Daniil Kireev <imstarosta4web@gmail.com>
2023-12-05 09:23:53 +0300

    Iter4 fix for default short url

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -14,7 +14,7 @@ func parseFlags() {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
 	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&flagShortRunAddr, "b", ":8080", "address and port to return short url")
+	flag.StringVar(&flagShortRunAddr, "b", "http://localhost:8080", "address and port to return short url")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
 }
