62a1a6a4529205ff3c1973ed3bd9e3030bce859a
Daniil Kireev <imstarosta4web@gmail.com>
2023-12-06 12:50:20 +0300

    iter5

---
+++ cmd/shortener/config.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"os"
+)
+
+// неэкспортированная переменная flagRunAddr содержит адрес и порт для запуска сервера
+var flagRunAddr string
+var flagShortRunAddr string
+
+// parseFlags обрабатывает аргументы командной строки
+// и сохраняет их значения в соответствующих переменных
+func parseFlags() {
+	// регистрируем переменную flagRunAddr
+	// как аргумент -a со значением :8080 по умолчанию
+	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&flagShortRunAddr, "b", "http://localhost:8080", "address and port to return short url")
+	// парсим переданные серверу аргументы в зарегистрированные переменные
+	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
+
+	if envShortRunAddr := os.Getenv("BASE_URL"); envShortRunAddr != "" {
+		flagShortRunAddr = envShortRunAddr
+	}
+}
